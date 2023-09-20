b085c1ccaa420fb911d27af077cb927192d4cf83
Sanket Banerjee <sanketbanerjee.2004@gmail.com>
2023-09-20 20:44:22 +0530

    App.py updated

--- Diabetes_Prediction_App.py
+++ Diabetes_Prediction_App.py
@@ -7,7 +7,7 @@ with open('style.css') as f:
 
 
 # loading the saved model
-loaded_model = pickle.load(open("F:/ML/Diabetis_Prediction/trained_model.sav", "rb"))
+loaded_model = pickle.load(open("trained_model.sav", "rb"))
 
 
 # creating a function for Prediction
